5621b488d203265237ca81b85233d86a95add226
mtfishman <mfishman@flatironinstitute.org>
2025-12-28 18:23:08 -0500

    Code style

--- ext/FunctionImplementationsLinearAlgebraExt/FunctionImplementationsLinearAlgebraExt.jl
+++ ext/FunctionImplementationsLinearAlgebraExt/FunctionImplementationsLinearAlgebraExt.jl
@@ -5,7 +5,7 @@ import LinearAlgebra as LA
 
 struct DiagonalStyle <: FI.AbstractMatrixStyle end
 FI.Style(::Type{<:LA.Diagonal}) = DiagonalStyle()
-const permuteddims_diag = FI.Implementation(FI.permuteddims, DiagonalStyle())
+const permuteddims_diag = DiagonalStyle()(FI.permuteddims)
 function permuteddims_diag(a::AbstractArray, perm)
     (ndims(a) == length(perm) && isperm(perm)) ||
         throw(ArgumentError("no valid permutation of dimensions"))
