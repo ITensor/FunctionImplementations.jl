32cccc785670885e9d991589c2d99348e5d863e5
mtfishman <mfishman@flatironinstitute.org>
2026-01-06 13:49:37 -0500

    Format

--- ext/FunctionImplementationsFillArraysExt/FunctionImplementationsFillArraysExt.jl
+++ ext/FunctionImplementationsFillArraysExt/FunctionImplementationsFillArraysExt.jl
@@ -4,9 +4,9 @@ using FillArrays: RectDiagonal
 using FunctionImplementations: FunctionImplementations
 
 function FunctionImplementations.permuteddims(a::RectDiagonal, perm)
-  (ndims(a) == length(perm) && isperm(perm)) ||
-    throw(ArgumentError("no valid permutation of dimensions"))
-  return RectDiagonal(parent(a), ntuple(d -> axes(a)[perm[d]], ndims(a)))
+    (ndims(a) == length(perm) && isperm(perm)) ||
+        throw(ArgumentError("no valid permutation of dimensions"))
+    return RectDiagonal(parent(a), ntuple(d -> axes(a)[perm[d]], ndims(a)))
 end
 
 end
